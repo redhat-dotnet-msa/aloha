7af2fc7ee5e267b466fe49baf8bc8e3d7229ed15
Don Schenck <don.schenck@gmail.com>
2017-06-08 15:19:36 -0400

    Another webhook-triggering minor edit.

--- HomeModule.cs
+++ HomeModule.cs
@@ -4,7 +4,7 @@ public class HomeModule : NancyModule
     {
         public HomeModule()
         {
-            Get("/", args => "Aloha from .NET, using the NancyFX framework.");
+            Get("/", args => "Aloha from .NET, using the NancyFX framework. This is version 2.0 of this program.");
 
             Get("/os", x =>
             {
